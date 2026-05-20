879a7247739bc27c9dfac251be979f3da95f2777
niiho-katsumi <amanojaku.ktm@gmail.com>
2026-05-21 00:14:33 +0900

    ThumbnailコンポーネントのimgタグをImageコンポーネントに置換

--- app/_components/atoms/Thumbnail.tsx
+++ app/_components/atoms/Thumbnail.tsx
@@ -1,13 +1,16 @@
 "use client";
+import Image from "next/image";
 type Props = {
   title: string;
   url: string;
+  width: number;
+  height: number;
 };
 
-export default function Thumbnail({ title, url }: Props) {
+export default function Thumbnail({ title, url, width, height }: Props) {
   return (
     <>
-      <img src={url} alt={title} />
+      <Image src={url} alt={title} width={width} height={height} />
     </>
   );
 }
